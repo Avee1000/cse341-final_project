f1dc4c5597bcff09429882ee2955875c3647c7c4
Avee1000 <eddyidahosa01@gmail.com>
2025-10-04 18:39:28 +0100

    Update user route to PUT method and add API documentation

--- routes/users-route.js
+++ routes/users-route.js
@@ -6,7 +6,7 @@ const isAuthenticated = require("../utilities/authenticate");
 
 router.get("/users/accounts", isAuthenticated, invCont.getAllUsers);
 
-router.get("/users/accounts/:id", isAuthenticated, invCont.editUsers)
+router.put("/users/accounts/:id", isAuthenticated, invCont.editUsers)
 
 
 module.exports = router;
